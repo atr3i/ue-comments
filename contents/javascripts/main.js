2272fb99d0cc4971277673f627470b7f76e180a8
Ana Trello <ana.trello@elmundo.es>
2017-09-15 16:34:11 +0200

    Cambios en buttons

--- contents/javascripts/main.js
+++ contents/javascripts/main.js
@@ -1,29 +1,5 @@
 // TOGGLE VISIBILITY
 
-    // var toggle_visibility = (function() {
-    //     function toggle(cl) {
-    //         var els = document.getElementsByClassName(cl);
-    //         for (var i = 0; i < els.length; ++i) {
-    //             var s = els[i].style;
-    //             s.display = s.display === 'none' ? 'block' : 'none';
-    //         };
-    //     }
-    //     return function(cl) {
-    //         if (cl instanceof Array) {
-    //             for (var i = 0; i < cl.length; ++i) {
-    //                 toggle(cl[i]);
-    //             }
-    //         } else {
-    //             toggle(cl);
-    //         }
-    //     };
-    // })();
-    // USAGE:
-    //  toggle_visibility('myclass');
-    //  toggle_visibility(['myclass1','myclass2','myclass3']);
-
-
-
 // Show an element
 var show = function (elem) {
   elem.classList.add('is-visible');
@@ -42,17 +18,17 @@ var toggle = function (elem) {
 // Listen for click events
 document.addEventListener('click', function (event) {
 
-  // Make sure clicked element is our toggle
+  //Make sure clicked element is our toggle
   if (!event.target.classList.contains('toggle')) return;
 
-  // Prevent default link behavior
+  //Prevent default link behavior
   event.preventDefault();
 
-  // Get the content
+  //Get the content
   var content = document.querySelector(event.target.hash);
   if (!content) return;
 
-  // Toggle the content
+  //Toggle the content
   toggle(content);
 
 }, false);
